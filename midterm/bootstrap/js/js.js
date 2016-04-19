4a568c2f3238be564aa110ec2669c7a3b673d4e4
yoyoe3 <yoyoe33@gamil.com>
2016-04-19 20:11:56 +0800

    新增首頁精選文章+幻燈片說明,AboutUs-footer

---
+++ midterm/bootstrap/js/js.js
@@ -0,0 +1,16 @@
+// This file is js by ourself
+function Firsttitleclick() {
+    document.getElementById("articalchange").innerHTML="無須在意別人的評論,<br>只要把自己的事情做好;<br>無須看別人的眼神,<br>只需走自己的路;<br>無須有過多的抱怨,<br>那樣會使自己的心更累。<br>不管走在何處,<br>都不能迷失自己>/////<";
+}
+
+function Secondtitleclick() {
+    document.getElementById("articalchange").innerHTML="要做自己很簡單的<br>大多數的人其實都不在乎你發什麼文<br>只有喜歡你的人才會在意你寫什麼<br>所以大可盡量的做自己<br>說你想說的<br><strong>反正也沒什麼人喜歡你</strong>";
+}
+
+function Thirdtitleclick() {
+    document.getElementById("articalchange").innerHTML="最佳的報復不是仇恨<br>而是打心底發出的冷淡<br>幹嘛花力氣<br>去恨一個不相干的人";
+}
+
+function Fourthtitleclick() {
+    document.getElementById("articalchange").innerHTML="人的一生要瘋狂一次<br>無論是為<br>一個人,<br>一段情<br>一段旅途,<br>或一個夢想.";
+}
